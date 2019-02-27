9d075d885da819df13503717240eb254c30015ef
Gilmour Almalbis <gilmouralmalbisdev@gmail.com>
2019-02-27 13:29:05 +0800

    control tab

---
+++ ConcessionaireReports/ConcessionaireReportsForm.Designer.cs
@@ -0,0 +1,93 @@
+﻿namespace ConcessionaireReports
+{
+    partial class ConcessionaireReportsForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tabControlConcessionaireReports = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.tabControlConcessionaireReports.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tabControlConcessionaireReports
+            // 
+            this.tabControlConcessionaireReports.Alignment = System.Windows.Forms.TabAlignment.Left;
+            this.tabControlConcessionaireReports.Controls.Add(this.tabPage1);
+            this.tabControlConcessionaireReports.Controls.Add(this.tabPage2);
+            this.tabControlConcessionaireReports.ItemSize = new System.Drawing.Size(35, 150);
+            this.tabControlConcessionaireReports.Location = new System.Drawing.Point(12, 12);
+            this.tabControlConcessionaireReports.Multiline = true;
+            this.tabControlConcessionaireReports.Name = "tabControlConcessionaireReports";
+            this.tabControlConcessionaireReports.SelectedIndex = 0;
+            this.tabControlConcessionaireReports.Size = new System.Drawing.Size(982, 489);
+            this.tabControlConcessionaireReports.SizeMode = System.Windows.Forms.TabSizeMode.Fixed;
+            this.tabControlConcessionaireReports.TabIndex = 0;
+            this.tabControlConcessionaireReports.DrawItem += new System.Windows.Forms.DrawItemEventHandler(this.tabControlConcessionaireReports_DrawItem);
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Location = new System.Drawing.Point(154, 4);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(824, 481);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "tabPage1";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Location = new System.Drawing.Point(154, 4);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(824, 481);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "tabPage2";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // ConcessionaireReportsForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1006, 513);
+            this.Controls.Add(this.tabControlConcessionaireReports);
+            this.Name = "ConcessionaireReportsForm";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.ConcessionaireReportsForm_Load);
+            this.tabControlConcessionaireReports.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tabControlConcessionaireReports;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+    }
+}
+
---
+++ ConcessionaireReports/ConcessionaireReportsForm.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ConcessionaireReports
+{
+    public partial class ConcessionaireReportsForm : Form
+    {
+        public ConcessionaireReportsForm()
+        {
+            InitializeComponent();
+        }
+
+        private void tabControlConcessionaireReports_DrawItem(object sender, DrawItemEventArgs e)
+        {
+            Graphics g = e.Graphics;
+            Brush _textBrush;
+
+            // Get the item from the collection.
+            TabPage _tabPage = tabControlConcessionaireReports.TabPages[e.Index];
+
+            // Get the real bounds for the tab rectangle.
+            Rectangle _tabBounds = tabControlConcessionaireReports.GetTabRect(e.Index);
+
+            if (e.State == DrawItemState.Selected)
+            {
+                // Draw a different background color, and don't paint a focus rectangle.
+                _textBrush = new SolidBrush(Color.Red);
+                g.FillRectangle(Brushes.Gray, e.Bounds);
+            }
+            else
+            {
+                _textBrush = new System.Drawing.SolidBrush(e.ForeColor);
+                e.DrawBackground();
+            }
+            // Use our own font.
+            Font _tabFont = new Font("Arial", (float)10.0, FontStyle.Bold, GraphicsUnit.Pixel);
+
+            // Draw string. Center the text.
+            StringFormat _stringFlags = new StringFormat();
+            _stringFlags.Alignment = StringAlignment.Center;
+            _stringFlags.LineAlignment = StringAlignment.Center;
+            g.DrawString(_tabPage.Text, _tabFont, _textBrush, _tabBounds, new StringFormat(_stringFlags));
+        }
+
+        private void ConcessionaireReportsForm_Load(object sender, EventArgs e)
+        {
+            tabControlConcessionaireReports.DrawMode = TabDrawMode.OwnerDrawFixed;
+        }
+    }
+}
