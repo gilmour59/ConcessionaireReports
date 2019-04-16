cf38377c89d5e1f06e1b8c76413649ee7d49eac4
Gilmour Almalbis <gilmouralmalbisdev@gmail.com>
2019-04-16 11:53:32 +0800

    added Main Menu

---
+++ ConcessionaireReports/ReportMenuForm.Designer.cs
@@ -0,0 +1,176 @@
+﻿namespace ConcessionaireReports
+{
+    partial class ReportMenuForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonConcessionaire = new System.Windows.Forms.Button();
+            this.buttonMeter = new System.Windows.Forms.Button();
+            this.buttonMeterReading = new System.Windows.Forms.Button();
+            this.buttonBilling = new System.Windows.Forms.Button();
+            this.buttonCollection = new System.Windows.Forms.Button();
+            this.buttonReceivable = new System.Windows.Forms.Button();
+            this.buttonARMaintenance = new System.Windows.Forms.Button();
+            this.buttonDisconRecon = new System.Windows.Forms.Button();
+            this.buttonJobOrders = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // buttonConcessionaire
+            // 
+            this.buttonConcessionaire.Font = new System.Drawing.Font("Consolas", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonConcessionaire.Location = new System.Drawing.Point(50, 41);
+            this.buttonConcessionaire.Name = "buttonConcessionaire";
+            this.buttonConcessionaire.Size = new System.Drawing.Size(150, 100);
+            this.buttonConcessionaire.TabIndex = 0;
+            this.buttonConcessionaire.Text = "Concessionaire";
+            this.buttonConcessionaire.UseVisualStyleBackColor = true;
+            this.buttonConcessionaire.Click += new System.EventHandler(this.buttonConcessionaire_Click);
+            // 
+            // buttonMeter
+            // 
+            this.buttonMeter.Font = new System.Drawing.Font("Consolas", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonMeter.Location = new System.Drawing.Point(50, 147);
+            this.buttonMeter.Name = "buttonMeter";
+            this.buttonMeter.Size = new System.Drawing.Size(150, 100);
+            this.buttonMeter.TabIndex = 0;
+            this.buttonMeter.Text = "Meter";
+            this.buttonMeter.UseVisualStyleBackColor = true;
+            this.buttonMeter.Click += new System.EventHandler(this.buttonMeter_Click);
+            // 
+            // buttonMeterReading
+            // 
+            this.buttonMeterReading.Font = new System.Drawing.Font("Consolas", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonMeterReading.Location = new System.Drawing.Point(50, 253);
+            this.buttonMeterReading.Name = "buttonMeterReading";
+            this.buttonMeterReading.Size = new System.Drawing.Size(150, 100);
+            this.buttonMeterReading.TabIndex = 0;
+            this.buttonMeterReading.Text = "Meter Reading";
+            this.buttonMeterReading.UseVisualStyleBackColor = true;
+            this.buttonMeterReading.Click += new System.EventHandler(this.buttonMeterReading_Click);
+            // 
+            // buttonBilling
+            // 
+            this.buttonBilling.Font = new System.Drawing.Font("Consolas", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonBilling.Location = new System.Drawing.Point(252, 41);
+            this.buttonBilling.Name = "buttonBilling";
+            this.buttonBilling.Size = new System.Drawing.Size(150, 100);
+            this.buttonBilling.TabIndex = 0;
+            this.buttonBilling.Text = "Billing";
+            this.buttonBilling.UseVisualStyleBackColor = true;
+            this.buttonBilling.Click += new System.EventHandler(this.buttonBilling_Click);
+            // 
+            // buttonCollection
+            // 
+            this.buttonCollection.Font = new System.Drawing.Font("Consolas", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonCollection.Location = new System.Drawing.Point(252, 147);
+            this.buttonCollection.Name = "buttonCollection";
+            this.buttonCollection.Size = new System.Drawing.Size(150, 100);
+            this.buttonCollection.TabIndex = 0;
+            this.buttonCollection.Text = "Collection";
+            this.buttonCollection.UseVisualStyleBackColor = true;
+            this.buttonCollection.Click += new System.EventHandler(this.buttonCollection_Click);
+            // 
+            // buttonReceivable
+            // 
+            this.buttonReceivable.Font = new System.Drawing.Font("Consolas", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonReceivable.Location = new System.Drawing.Point(252, 253);
+            this.buttonReceivable.Name = "buttonReceivable";
+            this.buttonReceivable.Size = new System.Drawing.Size(150, 100);
+            this.buttonReceivable.TabIndex = 0;
+            this.buttonReceivable.Text = "Receivable";
+            this.buttonReceivable.UseVisualStyleBackColor = true;
+            this.buttonReceivable.Click += new System.EventHandler(this.buttonReceivable_Click);
+            // 
+            // buttonARMaintenance
+            // 
+            this.buttonARMaintenance.Font = new System.Drawing.Font("Consolas", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonARMaintenance.Location = new System.Drawing.Point(461, 41);
+            this.buttonARMaintenance.Name = "buttonARMaintenance";
+            this.buttonARMaintenance.Size = new System.Drawing.Size(150, 100);
+            this.buttonARMaintenance.TabIndex = 0;
+            this.buttonARMaintenance.Text = "AR Maintenance";
+            this.buttonARMaintenance.UseVisualStyleBackColor = true;
+            this.buttonARMaintenance.Click += new System.EventHandler(this.buttonARMaintenance_Click);
+            // 
+            // buttonDisconRecon
+            // 
+            this.buttonDisconRecon.Font = new System.Drawing.Font("Consolas", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonDisconRecon.Location = new System.Drawing.Point(461, 147);
+            this.buttonDisconRecon.Name = "buttonDisconRecon";
+            this.buttonDisconRecon.Size = new System.Drawing.Size(150, 100);
+            this.buttonDisconRecon.TabIndex = 0;
+            this.buttonDisconRecon.Text = "Disconnection and Reconnection";
+            this.buttonDisconRecon.UseVisualStyleBackColor = true;
+            this.buttonDisconRecon.Click += new System.EventHandler(this.buttonDisconRecon_Click);
+            // 
+            // buttonJobOrders
+            // 
+            this.buttonJobOrders.Font = new System.Drawing.Font("Consolas", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonJobOrders.Location = new System.Drawing.Point(461, 253);
+            this.buttonJobOrders.Name = "buttonJobOrders";
+            this.buttonJobOrders.Size = new System.Drawing.Size(150, 100);
+            this.buttonJobOrders.TabIndex = 0;
+            this.buttonJobOrders.Text = "Job Orders";
+            this.buttonJobOrders.UseVisualStyleBackColor = true;
+            this.buttonJobOrders.Click += new System.EventHandler(this.buttonJobOrders_Click);
+            // 
+            // ReportMenuForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(669, 396);
+            this.Controls.Add(this.buttonJobOrders);
+            this.Controls.Add(this.buttonDisconRecon);
+            this.Controls.Add(this.buttonReceivable);
+            this.Controls.Add(this.buttonCollection);
+            this.Controls.Add(this.buttonARMaintenance);
+            this.Controls.Add(this.buttonMeterReading);
+            this.Controls.Add(this.buttonBilling);
+            this.Controls.Add(this.buttonMeter);
+            this.Controls.Add(this.buttonConcessionaire);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "ReportMenuForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Reports Main Menu";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonConcessionaire;
+        private System.Windows.Forms.Button buttonMeter;
+        private System.Windows.Forms.Button buttonMeterReading;
+        private System.Windows.Forms.Button buttonBilling;
+        private System.Windows.Forms.Button buttonCollection;
+        private System.Windows.Forms.Button buttonReceivable;
+        private System.Windows.Forms.Button buttonARMaintenance;
+        private System.Windows.Forms.Button buttonDisconRecon;
+        private System.Windows.Forms.Button buttonJobOrders;
+    }
+}
---
+++ ConcessionaireReports/ReportMenuForm.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ConcessionaireReports
+{
+    public partial class ReportMenuForm : Form
+    {
+        public ReportMenuForm()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonConcessionaire_Click(object sender, EventArgs e)
+        {
+            ConcessionaireReportsForm form = new ConcessionaireReportsForm();
+            form.ShowDialog();
+        }
+
+        private void buttonMeter_Click(object sender, EventArgs e)
+        {
+            MeterReportsForm form = new MeterReportsForm();
+            form.ShowDialog();
+        }
+
+        private void buttonMeterReading_Click(object sender, EventArgs e)
+        {
+            MeterReadingReportsForm form = new MeterReadingReportsForm();
+            form.ShowDialog();
+        }
+
+        private void buttonBilling_Click(object sender, EventArgs e)
+        {
+            BillingReportsForm form = new BillingReportsForm();
+            form.ShowDialog();
+        }
+
+        private void buttonCollection_Click(object sender, EventArgs e)
+        {
+            CollectionReportsForm form = new CollectionReportsForm();
+            form.ShowDialog();
+        }
+
+        private void buttonReceivable_Click(object sender, EventArgs e)
+        {
+            ReceivablesReportsForm form = new ReceivablesReportsForm();
+            form.ShowDialog();
+        }
+
+        private void buttonARMaintenance_Click(object sender, EventArgs e)
+        {
+            ARMaintenanceReportsForm form = new ARMaintenanceReportsForm();
+            form.ShowDialog();
+        }
+
+        private void buttonDisconRecon_Click(object sender, EventArgs e)
+        {
+            DisconReconReportsForm form = new DisconReconReportsForm();
+            form.ShowDialog();
+        }
+
+        private void buttonJobOrders_Click(object sender, EventArgs e)
+        {
+            JobOrdersReportsForm form = new JobOrdersReportsForm();
+            form.ShowDialog();
+        }
+    }
+}
